5a692e843cc5299d0d66b78e55aab493e647146e
Alexandr Styazhkov <alexsandr.styazhkov@gmail.com>
2020-11-17 01:26:49 +0600

    Добавьте файлы проекта.

---
+++ Models/Data/Db.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Store.Models.Data
+{
+    public class Db : DbContext
+    {
+
+    }
+}
---
+++ Models/ViewModels/Pages/PageVM.cs
@@ -0,0 +1,38 @@
+﻿using MVC_Store.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Store.Models.ViewModels.Pages
+{
+    public class PageVM
+    {
+        public PageVM()
+        {
+
+        }
+
+        public PageVM(PagesDTO row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Slug = row.Slug;
+            Body = row.Body;
+            Sorting = row.Sorting;
+            HasSidebar = row.HasSidebar;
+        }
+
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(int.MaxValue,MinimumLength = 3)]
+        public string Slug { get; set; }
+        public string Body { get; set; }
+        public string Sorting { get; set; }
+        public bool HasSidebar { get; set; }
+    }
+}
